943850f439cd560cd04a2a8dc540bee13523df8e
_[Burn]_ <baryan.acb@gmail.com>
2021-05-10 05:58:08 -0500

    cart

--- routes.js
+++ routes.js
@@ -42,5 +42,53 @@ routes.get('/products', (req, res)=>{
     })
 })
 
+routes.get('/cart', (req, res)=>{
+    req.getConnection((err, conn) => {
+        if(err) return res.send(err);
+
+        conn.query('select * from products t1 inner join cart t2 on t1.id_product = t2.id_product', (err, rows) =>{
+            if (err) return res.send(err);
+
+            res.json(rows)
+        })
+    })
+})
+
+routes.delete('/cart/:id', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err);        
+
+        conn.query('DELETE FROM cart WHERE id_cart = ?', [req.params.id], (err, rows)=>{
+            if(err) return res.send(err);
+
+            res.send('Cart excluded!')
+        })
+    })
+})
+
+routes.post('/cartEmpty', (req, res)=> {
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err);
+
+        conn.query('delete from cart' , (err, rows) => {
+            if (err) return res.send(err);
+
+            res.json(rows)
+        })
+    })
+})
+
+routes.put('/quantyCart/:id', (req, res)=>{
+    req.getConnection((err, conn) => {
+        if (err) return res.send(err);
+
+        conn.query('Update cart set ? where id_cart = ?', [req.body, req.params.id], (err, req) => {
+            if(err) return res.send(err)
+
+            res.send('book excluded!')
+        })
+    })
+})
+
 
 module.exports = routes
